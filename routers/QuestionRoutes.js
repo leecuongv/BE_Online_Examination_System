d56111fe9762a9842d4e06be75af19625f45179d
Lê Văn Cường <vancuongle65@gmail.com>
2022-11-13 23:30:52 +0700

    up

--- routers/QuestionRoutes.js
+++ routers/QuestionRoutes.js
@@ -6,5 +6,5 @@ const router = express.Router();
 router.post('/create-question', verifyToken, QuestionController.CreateQuestion);
 router.delete('', verifyToken, QuestionController.DeleteQuestion);
 router.post("/create-question-by-file", verifyToken, QuestionController.CreateQuestionByFile)
-
+router.post("/update-question-in-exam", verifyToken, QuestionController.UpdateQuestionInExam)
 module.exports = router;
