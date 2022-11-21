7d749d79aa2955a8bea9d4ad3f304a5ab7a8142d
Lê Văn Cường <vancuongle65@gmail.com>
2022-11-21 07:45:47 +0000

    statistic routes

--- routers/StatisticRoutes.js
+++ routers/StatisticRoutes.js
@@ -9,4 +9,10 @@ router.get("/number-of-courses", verifyToken, StatisticController.GetNumberOfCou
 router.get("/number-of-exams",verifyToken, StatisticController.GetNumberOfExams)
 router.get("/number-of-users", verifyToken, StatisticController.GetNumberOfUsers)
 router.get("/total-new-users-by-day", verifyToken, StatisticController.GetTotalNewUsersByDay)
+router.get("/list-bills", verifyToken, StatisticController.GetListBills)
+router.get("/list-bill-by-user", verifyToken, StatisticController.GetListBillByUser)
+router.get("/sum-revenue", verifyToken, StatisticController.GetSumRevenue)
+router.get("/total-revenue-by-day", verifyToken, StatisticController.GetTotalRevenueByDay)
+
+
 module.exports = router;
