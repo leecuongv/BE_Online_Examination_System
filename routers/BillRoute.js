c7655fff429027c45037cb99fc4abde39e8173ae
Lê Văn Cường <vancuongle65@gmail.com>
2023-01-04 22:25:06 +0700

    fix

--- routers/BillRoute.js
+++ routers/BillRoute.js
@@ -6,4 +6,7 @@ const router = express.Router();
 router.post("/create-payment/vnpay",verifyToken,BillController.CreatePaymentVNPay)
 router.get("/vnpay-return",BillController.VNPayIPN)
 
+router.post("/create-payment/momo",verifyToken,BillController.createPaymentMomo)
+router.get("/upgrade-momo",BillController.upgradeAccountWithMomo)
+
 module.exports = router;
