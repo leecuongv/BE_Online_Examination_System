bf6b80ea31b25d376e20307dc631fbc2216bc0ff
Lê Văn Cường <vancuongle65@gmail.com>
2023-03-07 00:03:56 +0700

    Update LessonRouters.js

---
+++ routers/LessonRouters.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const { verifyToken, verifyTokenAdmin } = require("../controllers/middlewareController")
+const { LessonController } = require('../controllers/LessonController')
+const router = express.Router();
+
+router.post('/create', verifyToken, LessonController.Create);
+
+router.put('/update', verifyToken, LessonController.Update);
+router.delete('/', verifyToken, LessonController.Delete);
+
+router.get('/Lesson-by-course-of-teacher', verifyToken, LessonController.getLessonByCourseOfTeacher)
+
+
+router.get('/by-slug', verifyToken, LessonController.getLessonBySlug);
+router.put('/public', verifyToken, LessonController.Public)
+router.put('/close', verifyToken, LessonController.Close)
+
+module.exports = router;
