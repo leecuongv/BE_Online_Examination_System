38350ffbe2522c7e11636bcc2b2ea5448349a603
Lê Văn Cường <vancuongle65@gmail.com>
2023-05-10 16:49:50 +0700

    update certificate controller

--- controllers/CertificateController.js
+++ controllers/CertificateController.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
 const User = require("../models/User")
-const { ROLES, STATUS } = require("../utils/enum")
+const { ROLES, STATUS, CERTIFICATION } = require("../utils/enum")
 const dotenv = require('dotenv')
 const axios = require('axios');
 const FormData = require('form-data');
 const tokenBot = 'bot5567501004:AAEFZl4XA8Fc1D92QrO0vpKGLytC5fN_wZs'
-const telegramURL = "https://api.telegram.org/file/"+tokenBot+"/documents/"
+const telegramURL = "https://api.telegram.org/file/" + tokenBot + "/documents/"
 const fs = require("fs")
 const { degrees, PDFDocument, rgb, StandardFonts } = require("pdf-lib");
 const fontkit = require("@pdf-lib/fontkit");
@@ -17,17 +17,191 @@ const CertificateController = {
     Create: async (req, res) => {
         try {
             const loginUsername = req.user?.sub
-            const { slug } = req.query
+            const { courseId } = req.query
             const loginUser = await User.findOne({ username: loginUsername })
             if (!loginUser) {
                 return res.status(400).json({ message: "Tài khoản không tồn tại!" })
             }
 
-            const course = await Course.findOne({ courseId: slug })
+            const course = await Course.aggregate([
+                {
+                    $match:
+
+                        { courseId: Number(courseId) }
+
+                },
+                {
+                    $facet: {
+                        'temp': [
+                            {
+                                $lookup: {
+                                    from: "take_exams",
+                                    let: { examIds: "$exams" },
+                                    pipeline: [
+                                        {
+                                            $match:
+                                            {
+                                                $expr:
+                                                {
+                                                    $and:
+                                                        [
+                                                            { $eq: ["$userId", mongoose.Types.ObjectId(loginUser.id)] },
+                                                            { $in: ["$examId", "$$examIds"] }
+                                                        ]
+                                                }
+                                            }
+                                        },
+                                        { $project: { _id: 0, countId: "$examId" } },
+                                    ],
+                                    as: "takeExams"
+                                }
+                            },
+                            {
+                                $lookup: {
+                                    from: "submit_assignments",
+                                    let: { assignmentIds: "$assignments" },
+                                    pipeline: [
+                                        {
+                                            $match:
+                                            {
+                                                $expr:
+                                                {
+                                                    $and:
+                                                        [
+                                                            { $eq: ["$creatorId", mongoose.Types.ObjectId(loginUser.id)] },
+                                                            { $in: ["$assignmentId", "$$assignmentIds"] }
+                                                        ]
+                                                }
+                                            }
+                                        },
+                                        { $project: { _id: 0, countId: "$assignmentId" } },
+                                    ],
+                                    as: "assigns"
+                                }
+                            },
+                            {
+                                $lookup: {
+                                    from: "seen_lessons",
+                                    let: { lessonIds: "$lessons" },
+                                    pipeline: [
+                                        {
+                                            $match:
+                                            {
+                                                $expr:
+                                                {
+                                                    $and:
+                                                        [
+                                                            { $eq: ["$creatorId", mongoose.Types.ObjectId(loginUser.id)] },
+                                                            { $in: ["$lessonId", "$$lessonIds"] }
+                                                        ]
+                                                }
+                                            }
+                                        },
+                                        { $project: { _id: 0, countId: "$lessonId" } },
+                                    ],
+                                    as: "seenLessons"
+                                }
+                            },
+                            {
+                                $project: {
+                                    'counts': { $concatArrays: ['$assigns', '$takeExams', '$seenLessons'] }
+                                }
+                            },
+                            {
+                                $unwind: {
+                                    path: "$counts",
+                                    preserveNullAndEmptyArrays: true
+                                }
+                            },
+                            {
+                                $group: {
+                                    _id: '$counts.countId', "doc": { "$first": "$_id" }
+                                }
+                            },
+                            {
+                                $group: {
+                                    _id: '$doc', count: { $sum: 1 }
+                                }
+                            }
+                        ],
+                        'main': [
+                            {
+                                // $match: { students: { $in: [mongoose.Types.ObjectId(student.id)] } }
+                                $match: { courseId: Number(courseId) }
+                            },
+                            {
+                                $project: {
+                                    'doc': '$$ROOT',
+                                    'total': {
+                                        $size:
+                                        {
+                                            $concatArrays: [
+                                                '$exams', { $ifNull: ['$assignments', []] }, { $ifNull: ['$lessons', []] }]
+                                        }
+                                    }
+                                }
+                            },
+
+                        ]
+                    }
+                },
+                {
+                    $project: {
+                        all: {
+                            $concatArrays: ["$temp", "$main"]
+                        }
+                    }
+                },
+                {
+                    $unwind: "$all"
+                },
+                {
+                    $group: {
+                        _id: "$all._id",
+                        doc: { '$last': '$all.doc' },
+                        count: { $sum: "$all.count" },
+                        total: { $sum: "$all.total" },
+
+                    }
+                },
+                {
+                    $project: {
+                        _id: 1,
+                        name: '$doc.name',
+                        image: '$doc.image',
+                        courseId: "$doc.courseId",
+                        exams: '$doc.exams',
+                        students: '$doc.students',
+                        description: '$doc.description',
+                        certification: "$doc.certification",
+                        count: 1,
+                        total: 1,
+                        endTime: "$doc.endTime",
+                        avg: { $cond: [{ $eq: ["$total", 0] }, "0", { "$divide": ["$count", "$total"] }] }
+                    }
+                }
+
+            ]);
+            const courseCert = course[0].certification
+            console.log(courseCert)
+            console.log(course[0].certification === CERTIFICATION.NOTALLOW)
             if (!course) {
                 return res.status(400).json({ message: "Khoá học không tồn tại!" })
             }
+            if (!course[0].students.find(item => item.toString() === loginUser.id.toString())) {//nếu chưa có sinh viên trên
+                return res.status(400).json({ message: "Học viên không thuộc khoá học" })
+            }
+            if (course[0].certification === CERTIFICATION.NOTALLOW)
+                return res.status(400).json({ message: "Khoá học không hỗ trợ cấp chứng chỉ!" })
             //TODO: điều kiện cấp chứng chỉ 
+            if (course[0].certification === CERTIFICATION.WHENDONE && course[0].avg < 0.8)
+                return res.status(400).json({ message: "Chưa đủ điều kiện cấp chứng chỉ" })
+            if (course[0].certification === CERTIFICATION.WHENCOURSEDONE)
+                if ((new Date(course[0].endTime)) > (new Date()) || course[0].avg < 0.8)
+                    return res.status(400).json({ message: "Chưa đủ điều kiện cấp chứng chỉ" })
+
+
+
 
             const certificate = await Certificate.findOne({ user: loginUser.id, course: course.id })
             if (certificate) {
@@ -72,7 +246,7 @@ const CertificateController = {
             const options = { day: '2-digit', month: 'long', year: 'numeric' };
             const formattedDate = newDate.toLocaleDateString('en-US', options);
             // Draw a string of text diagonally across the first page
-            const url = GenerateURL(loginUser.name + " " + course.name)
+            const url = GenerateURL(loginUser.name + " " + course[0].name)
 
             firstPage.drawText(loginUser.fullname, {
                 x: 80,
@@ -81,14 +255,14 @@ const CertificateController = {
                 font: embedFontName,
                 color: rgb(0.36, 0.54, 0.66),
             });
-            firstPage.drawText(`Đã hoàn thành khoá học "` + course.name + `"`, {
+            firstPage.drawText(`Đã hoàn thành khoá học "` + course[0].name + `"`, {
                 x: 35,
                 y: 210,
                 size: 18,
                 font: embedFontCourse,
                 color: rgb(0, 0, 0),
             });
-            firstPage.drawText(`Has successfully completed the course  "` + course.name + `"`, {
+            firstPage.drawText(`Has successfully completed the course  "` + course[0].name + `"`, {
                 x: 35,
                 y: 180,
                 size: 17,
@@ -130,8 +304,8 @@ const CertificateController = {
             bodyFormData.append('document', fs.createReadStream("newfile.pdf"))
             console.log("Đọc file")
             let linkFile = ""
-            
-             axios.post(`https://api.telegram.org/${tokenBot}/sendDocument`,
+
+            axios.post(`https://api.telegram.org/${tokenBot}/sendDocument`,
                 bodyFormData,
                 {
                     headers: {
@@ -161,7 +335,7 @@ const CertificateController = {
                                     message: "Tạo chứng chỉ không thành công!"
                                 })
                             }
-                
+
                             const cert = await newCert.save();
                             return res.status(200).json({
                                 message: "Tạo chứng chỉ thành công!",
@@ -182,9 +356,9 @@ const CertificateController = {
                         message: 'Tạo file không thành công'
                     })
                 });
-                
 
-            
+
+
 
         } catch (error) {
             console.log(error)
